03ae2754a135a2654c5b3d29740814e1fa853c9d
vinodgoguru <gvinodkumar900@gmail.com>
2026-05-22 18:43:45 +0530

    second commit

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Trump: Hey Iran, we are going to blast you!"
+echo "Iran: Yes, please. You are welcome"
+echo "Trump: Hey Iran, I am serious"
+echo "Iran: Hey Trump, we are also serious"
