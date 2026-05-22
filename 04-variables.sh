215441511b195ce4459d5c4d4826f8b9de82b989
vinodgoguru <gvinodkumar900@gmail.com>
2026-05-22 19:04:23 +0530

    second commit

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,3 +4,8 @@ echo "Please enter your usernane::"
 read USER_NAME # here USER_NAME is variable, whatever you enter in terminal stores in this variabl
 
 echo "Username is $USER_NAME"
+
+echo "Please enter your password::"
+read -s PASSWORD
+
+echo "Password is : $PASSWORD"
