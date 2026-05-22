bb2931d942d9949ed7cfc4ed4cc385a1c0cb584c
vinodgoguru <gvinodkumar900@gmail.com>
2026-05-22 18:56:22 +0530

    second commit

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hey $PERSON2, we are going to blast you!"
+echo "$PERSON2: Yes, please. You are welcome"
+echo "$PERSON1: Hey $PERSON2, I am serious"
+echo "$PERSON2: Hey $PERSON1, we are also serious"
