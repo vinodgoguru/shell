16b3c79e3e39a9b5d348e5dc59a9eddb126ab013
vinodgoguru <gvinodkumar900@gmail.com>
2026-05-22 18:49:33 +0530

    second commit

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Trump: Hey Iran, we are going to blast you!"
-echo "Iran: Yes, please. You are welcome"
-echo "Trump: Hey Iran, I am serious"
-echo "Iran: Hey Trump, we are also serious"
+PERSON1=Trump
+PERSON2=Iran
+
+echo "$PERSON1: Hey $PERSON2, we are going to blast you!"
+echo "$PERSON2: Yes, please. You are welcome"
+echo "$PERSON1: Hey $PERSON2, I am serious"
+echo "$PERSON2: Hey $PERSON1, we are also serious"
