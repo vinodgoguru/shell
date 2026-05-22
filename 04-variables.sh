36c1e2696eb474268bc8195d6e8a6f790c9c7413
vinodgoguru <gvinodkumar900@gmail.com>
2026-05-22 19:02:14 +0530

    second commit

---
+++ 04-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Please enter your usernane::"
+read USER_NAME # here USER_NAME is variable, whatever you enter in terminal stores in this variabl
+
+echo "Username is $USER_NAME"
