37c76c72a9764ecefa36c564649017371495b364
vinodgoguru <gvinodkumar900@gmail.com>
2026-05-23 16:58:34 +0530

    third commit

--- 08-condition.sh
+++ 08-condition.sh
@@ -10,5 +10,9 @@ NUMBER=$1
 # le - less than or equal
 
 if [ $NUMBER -ge 20 ]; then
-    echo "Given number $NUMBER is greater than or equal to 20"
+    echo "Given number $NUMBER is greater than 20"
+elif [ $NUMBER -gt 20 ]
+    echo "Given number $NUMBER is equal to 20"  
+else
+    echo "Give number $NUMBER is less than 20"      
 fi    
