00ae5f4909e88e947ea74c9ed21d3533e0e0de2c
vinodgoguru <gvinodkumar900@gmail.com>
2026-05-23 15:14:51 +0530

    third commit

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -4,4 +4,16 @@
 
 echo "All variables passed to script: $@"
 echo "Number of variables passed: $#"
-echo " First variable: $1"
+echo "First variable: $1"
+echo "Script name: $0"
+echo "Who is running this : $USER"
+echo "Which directory: $PWD"
+echo "Home directory: $HOME"
+echo "PID of current script: $$"
+sleep 5 &
+echo "PID of background command running just now : $!"
+wait $!
+echo "Line number: $LINENO"
+echo "Script executed in $SECONDS seconds"
+echo "Random Number: $RANDOM"
+echo "Exit code of previous command: $?"
