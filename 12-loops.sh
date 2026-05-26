8efbf9eeb1841aae6670bb744fcce19ef829fc3c
jayasrinagunoori <nagunoorijayasri1124@gmail.com>
2026-05-26 12:27:37 +0530

    fifth commit

---
+++ 12-loops.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log" 
+
+# Check root access or not
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root acess"
+    exit 1
+fi
+
+# First arg --> what are you trying to install
+# Second arg --> exit code
+
+VALIDATE(){
+    if [ $? -ne 0 ]; then
+        echo " Installing $1 is ...failed "
+    else
+        echo " Installing $1 is .. Success"
+    fi
+
+}
+
+for package in $@
+do 
+    echo "Installing $package"
+done
+
+
+
